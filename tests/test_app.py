841bd58f828d78b65fd5c12e1e8e95a5e1ae25e1
Marcel Hild <hild@b4mad.net>
2020-06-01 19:56:43 +0200

    bot shifts until no more moves

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,19 +4,18 @@ from threesus.bots import RandomBot
 def test_game():
     game = Game()
     bot = RandomBot()
+    shifted = False
 
     print("\nStart Board")
     print(f"{game.board}\n")
 
     while True:
-        next_move = bot.get_next_move(game.board, game.deck, game.next_card_hint())
-        print(f"shift {next_move}")
-
+        next_move = bot.get_next_move(game.board, game.deck, game.next_card_hint(), shifted)
         if not next_move:
+            print(f"No more moves")
             break
-        shifted = game.shift(next_move)
 
-        if not shifted:
-            break
+        print(f"shift {next_move}")
+        shifted = game.shift(next_move)
 
         print(f"new Board:\n{game.board}\n")
--- threesus/bots/random_bot.py
+++ threesus/bots/random_bot.py
@@ -3,9 +3,23 @@ import random
 class RandomBot(object):
 
     def __init__(self):
-        pass
+        self.valid_directions = []
+        self.last_move = None
 
-    def get_next_move(self, board, deck, next_card_hint):
-        valid_directions = board.all_shift_directions()
-        # return valid_directions[0]
-        return random.choice(valid_directions)
+    def get_next_move(self, board, deck, next_card_hint, previous_shifted):
+        if not previous_shifted and self.last_move:
+            self.valid_directions.remove(self.last_move)
+        else:
+            self.update_valid_directions(board)
+
+        if self.valid_directions:
+            move = random.choice(self.valid_directions)
+            self.last_move = move
+            return move
+
+        return None
+    
+    def update_valid_directions(self, board):
+        self.valid_directions = board.all_shift_directions()
+
+
